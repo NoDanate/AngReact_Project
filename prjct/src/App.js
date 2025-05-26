3fe1ba36794cde6e7f760d9c699e4a343e3ef26a
NoDanate <fdor.peshkov@bk.ru>
2025-05-26 21:26:39 +0500

    Оболочка для тестов

--- prjct/src/App.js
+++ prjct/src/App.js
@@ -1,6 +1,9 @@
 import React, {useState} from "react";
 import './App.css';
 
+const matchAudio = new Audio('');
+const mismatchAudio = new Audio('');
+
 function App() {
   const cardEmojies = [
     '🐶', '🐱', '🐭', '🐹', '🐰', '🦊',
@@ -8,12 +11,11 @@ function App() {
     '🐷', '🐸', '🐵', '🐔', '🐧', '🐦'
   ];
 
-  const matchAudio = new Audio('');
-  const mismatchAudio = new Audio('');
-
   const [gridSize, setGridSize] = useState(4);
 
   const [cards, setCards] = useState([]);
+
+  const [disabled, setDisabled] = useState(false)
 
   const [flipped, setFlipped] = useState([]);
 
@@ -54,10 +56,75 @@ function App() {
     setSolved([]);
   }
 
+  const handleCardClick = (id) => {
+    if(disabled || flipped.includes(id) || solved.includes(id) || !gameStarted) return;
+
+    const newFlipped = [...flipped, id];
+    setFlipped(newFlipped);
+
+    setFlips(flips + 1);
+
+    if (newFlipped.length === 2){
+      setDisabled(true);
+      checkForMatch(newFlipped);
+    }
+  }
+
+  const checkForMatch = (flippedCards) => {
+    const [first, second] = flippedCards;
+    const card1 = cards.find(card => card.id === first);
+    const card2 = cards.find(card => card.id === second);
+
+    if (card1.emodji === card2.emodji){
+      setSolved([...solved, first, second]);
+      setFlipped([]);
+      setDisabled(false);
+
+      matchAudio.play();
+    }
+    else{
+      setTimeout(() => {
+        setFlipped([]);
+        setDisabled(false);
+      }, 1000);
+      mismatchAudio.play();
+    }
+  }
+
+
 
   return (
-      <div>
+      <div className='memory-game'>
+        <h1>Игра Память</h1>
+        <div>
+          <p>Размер сетки</p>
+          <select
+              value={gridSize}
+              onChange={(e) => setGridSize(e.target.value)}
 
+          >
+            <option value='4'>4x4</option>
+            <option value='6'>6x6</option>
+          </select>
+        </div>
+        <button onClick={initGame}>
+          {gameStarted ? 'Игра начата' : 'Начать игру'}
+        </button>
+        <button onClick={''}>Выйти</button>
+
+        <div className='game-stats'>
+          <p>Время: </p>
+          <p>Количество переворотов: {flips}</p>
+          <p>Найдено пар: {solved.length / 2} из {(gridSize * gridSize) / 2}</p>
+        </div>
+
+        <div className='game-section' style={{gridTemplateColumns: `repeat(${gridSize}), 1fr`}}>
+          {cards.map(card => (
+              <div onClick={handleCardClick} key={card.id} className={`card ${flipped.includes(card.id) || solved.includes(card.id) ? 'flipped' : ''}`}>
+                {flipped.includes(card.id) || solved.includes(card.id) ? card.emodji : '?'}
+              </div>
+          ))}
+        </div>
       </div>
   );
 }
